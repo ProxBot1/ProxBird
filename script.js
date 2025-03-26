b59462e4281fff66911cbc31a85590632b1bd191
ProxBot1 <toofygamer26@gmail.com>
2025-03-26 23:10:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,28 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const bird = {
+    x: 50,
+    y: canvas.height / 2,
+    width: 25,
+    height: 25,
+};
+
+const pipes = [];
+const pipeWidth = 25;
+let pipeGap = 200;
+
 function drawBird() {
     ctx.fillStyle = 'gray';
-    ctx.fillRect(50, canvas.height / 2, 25, 25);
+    ctx.fillRect(bird.x, bird.y, bird.width, bird.height);
 }
 
 function drawPipes() {
-    ctx.fillStyle = 'green';
-    ctx.fillRect(canvas.width, 0, 25, canvas.height / 2);
-    ctx.fillRect(canvas.width, canvas.height / 2 + 200, 25, canvas.height / 2 - 200);
+    for (let pipe of pipes) {
+        ctx.fillStyle = 'green';
+        ctx.fillRect(pipe.x, 0, pipeWidth, pipe.topHeight);
+        ctx.fillRect(pipe.x, pipe.topHeight + pipeGap, pipeWidth, canvas.height - pipe.topHeight - pipeGap);
+    }
 }
 
 function gameLoop() {
@@ -23,5 +36,12 @@ function gameLoop() {
 
     requestAnimationFrame(gameLoop);
 }
+
+// Generate initial pipe
+const topHeight = Math.random() * (canvas.height - pipeGap - 100) + 50;
+pipes.push({
+    x: canvas.width,
+    topHeight: topHeight,
+});
 
 gameLoop();
